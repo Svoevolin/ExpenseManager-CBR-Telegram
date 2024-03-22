4c1eb15e842b43121e681454df79b1883fbd8333
Svoevolin <vanish@internet.ru>
2024-03-22 11:03:48 +0300

    bot works

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"github.com/Svoevolin/workshop_1_bot/internal/clients/tg"
+	"github.com/Svoevolin/workshop_1_bot/internal/config"
+	"github.com/Svoevolin/workshop_1_bot/internal/model/messages"
+	"log"
+)
 
+func main() {
+	config, err := config.New()
+	if err != nil {
+		log.Fatal("config init failed:", err)
+	}
+
+	tgClient, err := tg.New(config)
+	if err != nil {
+		log.Fatal("tg client init failed:", err)
+	}
+
+	msgModel := messages.New(tgClient)
+
+	tgClient.ListenUpdates(msgModel)
 }
