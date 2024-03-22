309d409319052ed30da9317804b4d067ec14f27b
Svoevolin <vanish@internet.ru>
2024-03-22 23:43:25 +0300

    Revert "didnt precommit"
    This reverts commit b0528f5ec80cbaa33f8064ad72b048127c85a187.

--- internal/clients/tg/tgclient.go
+++ internal/clients/tg/tgclient.go
@@ -36,37 +36,17 @@ func (c *Client) SendMessage(text string, userID int64) error {
 	return nil
 }
 
-func CreateInlineMenu(buttons *orderedmap.OrderedMap) tgbotapi.InlineKeyboardMarkup {
+func (c *Client) SendInlineMenu(text string, userID int64, buttons *orderedmap.OrderedMap) error {
 	tgButtonsRows := make([][]tgbotapi.InlineKeyboardButton, 0, 100)
 	for pair := buttons.Oldest(); pair != nil; pair = pair.Next() {
 		tgButtonsRows = append(tgButtonsRows,
 			tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData(pair.Key.(string), pair.Value.(string))))
 	}
-	return tgbotapi.NewInlineKeyboardMarkup(tgButtonsRows...)
-}
-
-func (c *Client) SendInlineMenu(text string, userID int64, buttons *orderedmap.OrderedMap) error {
 	newMessage := tgbotapi.NewMessage(userID, text)
-	newMessage.ReplyMarkup = CreateInlineMenu(buttons)
+	newMessage.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(tgButtonsRows...)
 	_, err := c.client.Send(newMessage)
 	if err != nil {
 		return errors.Wrap(err, "client.Send")
-	}
-	return nil
-}
-
-func (c *Client) EditTextAndMarkup(text string, userID int64, messageID int, buttons *orderedmap.OrderedMap) error {
-	_, err := c.client.Send(tgbotapi.NewEditMessageTextAndMarkup(userID, messageID, text, CreateInlineMenu(buttons)))
-	if err != nil {
-		return errors.Wrap(err, "client.Send")
-	}
-	return nil
-}
-
-func (c *Client) DeleteMessage(userID int64, messageID int) error {
-	_, err := c.client.Request(tgbotapi.NewDeleteMessage(userID, messageID))
-	if err != nil {
-		return errors.Wrap(err, "client.Request")
 	}
 	return nil
 }
@@ -81,7 +61,7 @@ func (c *Client) ListenUpdates(msgModel *messages.Model) {
 
 	for update := range updates {
 		if update.Message != nil {
-			log.Printf("<message>[%s][%d] %s", update.Message.From.UserName, update.Message.From.ID, update.Message.Text)
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 			err := msgModel.IncomingMessage(messages.Message{
 				Text:   update.Message.Text,
@@ -89,18 +69,6 @@ func (c *Client) ListenUpdates(msgModel *messages.Model) {
 			})
 			if err != nil {
 				log.Println("error processing message:", err)
-			}
-		} else if update.CallbackQuery != nil {
-			log.Printf("<callback>[%s][%d] %s", update.CallbackQuery.From.UserName,
-				update.CallbackQuery.From.ID, update.CallbackQuery.Data)
-
-			err := msgModel.IncomingCallback(messages.CallBack{
-				UserID:    update.CallbackQuery.From.ID,
-				MessageID: update.CallbackQuery.Message.MessageID,
-				Data:      update.CallbackQuery.Data,
-			})
-			if err != nil {
-				log.Println("error processing callback:", err)
 			}
 		}
 	}
