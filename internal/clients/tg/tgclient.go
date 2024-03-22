d91e922398537840e998b22f55d0c8e4b13d62b0
Svoevolin <vanish@internet.ru>
2024-03-22 10:32:42 +0300

    client

--- internal/clients/tg/tgclient.go
+++ internal/clients/tg/tgclient.go
@@ -1,8 +1,10 @@
 package tg
 
 import (
+	"github.com/Svoevolin/workshop_1_bot/internal/model/messages"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/pkg/errors"
+	"log"
 )
 
 type TokenGetter interface {
@@ -25,5 +27,32 @@ func New(tokenGetter TokenGetter) (*Client, error) {
 }
 
 func (c *Client) SendMessage(text string, userID int64) error {
+	_, err := c.client.Send(tgbotapi.NewMessage(userID, text))
+	if err != nil {
+		return errors.Wrap(err, "client.Send")
+	}
+	return nil
+}
 
+func (c *Client) ListenUpdates(msgModel *messages.Model) {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := c.client.GetUpdatesChan(u)
+
+	log.Println("listening for messages")
+
+	for update := range updates {
+		if update.Message != nil {
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			err := msgModel.IncomingMessage(messages.Message{
+				Text:   update.Message.Text,
+				UserID: update.Message.From.ID,
+			})
+			if err != nil {
+				log.Println("error processing message:", err)
+			}
+		}
+	}
 }
