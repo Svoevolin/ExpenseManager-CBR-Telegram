65174e5d35f6f827128f71c88875b6a7d147f4a9
Svoevolin <vanish@internet.ru>
2024-03-22 10:11:33 +0300

    client

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/Svoevolin/workshop_1_bot
 go 1.20
 
 require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 	github.com/golang/mock v1.6.0
+	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.9.0
 )
 
---
+++ internal/clients/tg/tgclient.go
@@ -0,0 +1,29 @@
+package tg
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/pkg/errors"
+)
+
+type TokenGetter interface {
+	Token() string
+}
+
+type Client struct {
+	client *tgbotapi.BotAPI
+}
+
+func New(tokenGetter TokenGetter) (*Client, error) {
+	client, err := tgbotapi.NewBotAPI(tokenGetter.Token())
+	if err != nil {
+		return nil, errors.Wrap(err, "NewBotAPI")
+	}
+
+	return &Client{
+		client: client,
+	}, nil
+}
+
+func (c *Client) SendMessage(text string, userID int64) error {
+
+}
